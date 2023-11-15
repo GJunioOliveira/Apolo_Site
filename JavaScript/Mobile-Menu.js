d8fe1a24542910ba55ac4f449b2bfed9ee0e519f
G Junio <gabrielolivjunio098@gmail.com>
2023-11-15 19:20:05 +0000

    menu-hamburger-mudança

--- JavaScript/Mobile-Menu.js
+++ JavaScript/Mobile-Menu.js
@@ -1,48 +1,47 @@
-class MobileMenu{
-    constructor(mobileMenu, navList, navLinks){
+class MobileMenu {
+    constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
         this.activeClass = "active";
 
         this.handleClick = this.handleClick.bind(this);
-    } //Cons
-
-    animateLinks(){
-        this.navLinks.forEach((link, index)=> {
-            console.log(index)
-                link.style.animation
-                ? (link.style.animation = "")
-                : (link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7+ 0.3}s');
-        }); //animate
     }
 
-    handleClick(){
-        console.log(this)
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            console.log(index);
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
+    }
+
+    toggleMenu() {
         this.navList.classList.toggle(this.activeClass);
+    }
+
+    handleClick() {
+        this.toggleMenu();
         this.animateLinks();
-    }//handle 
+    }
 
-    addClickEvent(){
+    addClickEvent() {
         this.mobileMenu.addEventListener("click", this.handleClick);
-    
-    } //ClickEvent
+    }
 
-    init(){
-        if(this.mobileMenu){
+    init() {
+        if (this.mobileMenu) {
             this.addClickEvent();
-        } //if
-            return this;
-    } //init
-
-} //MobileMenu
+        }
+        return this;
+    }
+}
 
 const mobileMenu = new MobileMenu(
     ".menu-mobile",
     ".menu-nav",
-    ".menu-nav > li",
-
-
-); //Const
+    ".menu-nav > li"
+);
 
 mobileMenu.init();
